332599a8d5a8b5179566f3087598ded0508d16c0
Avishek Choudhary <56909326+avishekchoudhary@users.noreply.github.com>
2021-07-09 23:40:16 +0530

    Update main.py

--- coffee-and-wifi/main.py
+++ coffee-and-wifi/main.py
@@ -10,7 +10,7 @@ from wtforms.validators import InputRequired
 import csv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '#'
 Bootstrap(app)
 
 
